91714ec4dcc10489e1e68a137ac638c3dedd2c97
Leonardo Philippe <leonardopsgomes@gmail.com>
2021-03-16 22:08:45 -0400

    1- Curso JavaScript

---
+++ 1-meuPrimeiroProjeto.js
@@ -0,0 +1,9 @@
+console.log("Meu Primeiro Projeto: trabalhando com variaveis");
+
+const idade = 27;
+
+console.log("Este é a primeira idade: ",idade);
+console.log("Este é o somatorio de idade: ",idade+23);
+
+const idadeSomada = idade+2;
+console.log("A idade somada é:",idadeSomada);
---
+++ 4-conversaodeTipos.js
@@ -0,0 +1,8 @@
+console.log("Conversão de Tipos");
+
+console.log("ano" + 2020);
+console.log("2"+"2");
+console.log(parseInt("2")+parseInt("4"));
+
+
+console.log("2"/"4");
